367619f67fdb98d074dcf4218fe48b3c39f44b90
Joshua Howard <iamjoshhoward@gmail.com>
2024-07-02 06:30:37 -0400

    30th commit - default env variables

--- finance_analyzer/settings.py
+++ finance_analyzer/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Reads the .env file
 # Security settings
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1, finance-analyzer.azurewebsites.net'])
 
 # API Key from environment variable
 ALPHAVANTAGE_API_KEY = env('ALPHAVANTAGE_API_KEY')
